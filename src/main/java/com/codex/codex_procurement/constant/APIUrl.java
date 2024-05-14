9ec69b3fed1122bde6cf2ae9a63cfdf2e357522a
Muhammad Shabri rabbani <shabri@mhs.unsyiah.ac.id>
2024-05-14 16:55:35 +0700

    Initial commit

--- src/main/java/com/codex/codex_procurement/constant/APIUrl.java
+++ src/main/java/com/codex/codex_procurement/constant/APIUrl.java
@@ -3,5 +3,5 @@ package com.codex.codex_procurement.constant;
 public class APIUrl {
     public static final String PATH_VAR_ID = "/{id}";
     public static final String VENDOR_API = "/api/v1/vendors";
-    public static final String PRODUCT_API = "/api/vq/products";
+    public static final String PRODUCT_API = "/api/v1/products";
 }
