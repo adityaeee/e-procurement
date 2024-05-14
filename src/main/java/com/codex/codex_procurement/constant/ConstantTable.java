6a80d27a366b292767f6d80f739c6f0f2fa6f88c
M Rifky Aditya <rifkyaditya966@gmail.com>
2024-05-14 17:14:48 +0700

    [CREATE] constan table,api,response message

--- src/main/java/com/codex/codex_procurement/constant/ConstantTable.java
+++ src/main/java/com/codex/codex_procurement/constant/ConstantTable.java
@@ -3,4 +3,8 @@ package com.codex.codex_procurement.constant;
 public class ConstantTable {
     public static final String VENDOR = "vendor";
     public static final String PRODUCT = "product";
+    public static final String CATEGORY = "category";
+    public static final String TRANSACTION = "transaction";
+    public static final String VENDOR_PRODUCT = "vendor_product";
+
 }
