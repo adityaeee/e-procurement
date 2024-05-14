26eaf258aa49811e50bc3a851823cc1458b5c179
Muhammad Shabri rabbani <shabri@mhs.unsyiah.ac.id>
2024-05-15 00:33:33 +0700

    [ADD] vendor implements

--- src/main/java/com/codex/codex_procurement/service/VendorProductService.java
+++ src/main/java/com/codex/codex_procurement/service/VendorProductService.java
@@ -1,4 +1,9 @@
 package com.codex.codex_procurement.service;
 
+import com.codex.codex_procurement.entity.VendorProduct;
+
+import java.util.List;
+
 public interface VendorProductService {
+    List<VendorProduct> createBulk (List<VendorProduct> vendorProducts);
 }
--- src/main/java/com/codex/codex_procurement/service/VendorService.java
+++ src/main/java/com/codex/codex_procurement/service/VendorService.java
@@ -1,4 +1,20 @@
 package com.codex.codex_procurement.service;
 
+import com.codex.codex_procurement.dto.request.NewVendorRequest;
+import com.codex.codex_procurement.dto.request.SearchVendorRequest;
+import com.codex.codex_procurement.dto.request.VendorRequest;
+import com.codex.codex_procurement.dto.response.VendorResponse;
+import com.codex.codex_procurement.entity.Vendor;
+import org.springframework.data.domain.Page;
+
+
 public interface VendorService {
+    VendorResponse create(VendorRequest vendorRequest);
+    Vendor getById(String id);
+    VendorResponse getByIdListProduct (String id);
+    Page<Vendor> getAll(SearchVendorRequest vendorRequest);
+    Vendor update(Vendor vendor);
+    void delete(String id);
+
+
 }
