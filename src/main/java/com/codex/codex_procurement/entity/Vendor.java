ef0a1b796b6ccd34c6cbaf04aa90799c6f98f72d
Muhammad Shabri rabbani <shabri@mhs.unsyiah.ac.id>
2024-05-14 20:35:11 +0700

    [MODIFY] vendor and vendorproduct entity

--- src/main/java/com/codex/codex_procurement/entity/Vendor.java
+++ src/main/java/com/codex/codex_procurement/entity/Vendor.java
@@ -1,8 +1,18 @@
 package com.codex.codex_procurement.entity;
 
+import com.codex.codex_procurement.constant.ConstantTable;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
+@Table(name = ConstantTable.VENDOR)
 public class Vendor {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
