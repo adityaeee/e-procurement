bfca418f241cc9ad4afa2dd6a51da5c2783c511e
Muhammad Shabri rabbani <shabri@mhs.unsyiah.ac.id>
2024-05-14 20:21:46 +0700

    [ADD] vendor and vendorProduct entity

--- src/main/java/com/codex/codex_procurement/entity/VendorProduct.java
+++ src/main/java/com/codex/codex_procurement/entity/VendorProduct.java
@@ -1,13 +1,24 @@
 package com.codex.codex_procurement.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class VendorProduct {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "vendor_id")
+    private Vendor vendor;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "price")
+    private Long price;
+
 }
