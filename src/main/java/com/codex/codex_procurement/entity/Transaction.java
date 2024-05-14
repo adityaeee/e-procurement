ffbd84e2b5db57cb361a22f65cd058ce2fb878bd
Ahmad Ilham <ahmadelham21@gmail.com>
2024-05-14 20:24:16 +0700

    [ADD]entity table transaction

--- src/main/java/com/codex/codex_procurement/entity/Transaction.java
+++ src/main/java/com/codex/codex_procurement/entity/Transaction.java
@@ -1,13 +1,37 @@
 package com.codex.codex_procurement.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.codex.codex_procurement.constant.ConstantTable;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = ConstantTable.TRANSACTION)
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Column(name = "quantity",nullable = false,columnDefinition = "INT CHECK (quantity > 0)")
+    private Integer quantity;
+
+    @Column(name = "total_price",nullable = false,columnDefinition = "BIGINT CHECK (total_price >= 0)")
+    private Long totalPrice;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "trans_date", updatable = false)
+    private Date transDate;
+
+    @ManyToOne
+    @JoinColumn(name = "vendor_product_id")
+    @JsonBackReference
+    private VendorProduct vendorProduct;
 }
