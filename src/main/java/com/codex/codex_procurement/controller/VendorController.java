01667a15ff4e3ed03302b937a395443070f33940
Muhammad Shabri rabbani <shabri@mhs.unsyiah.ac.id>
2024-05-15 13:47:13 +0700

    [MODIFY] vendor controller

--- src/main/java/com/codex/codex_procurement/controller/VendorController.java
+++ src/main/java/com/codex/codex_procurement/controller/VendorController.java
@@ -10,10 +10,7 @@ import com.codex.codex_procurement.service.VendorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,4 +32,6 @@ public class VendorController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+
 }
