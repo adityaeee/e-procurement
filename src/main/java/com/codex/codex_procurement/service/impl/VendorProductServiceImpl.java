c87a46d369ce4854e7f37d7215b1aa0786f3844c
M Rifky Aditya <rifkyaditya966@gmail.com>
2024-05-15 10:06:14 +0700

    [MODIFY] vendor product service implement

--- src/main/java/com/codex/codex_procurement/service/impl/VendorProductServiceImpl.java
+++ src/main/java/com/codex/codex_procurement/service/impl/VendorProductServiceImpl.java
@@ -1,10 +1,13 @@
 package com.codex.codex_procurement.service.impl;
 
+import com.codex.codex_procurement.entity.Vendor;
 import com.codex.codex_procurement.entity.VendorProduct;
 import com.codex.codex_procurement.repository.VendorProductRepository;
 import com.codex.codex_procurement.service.VendorProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -18,4 +21,13 @@ public class VendorProductServiceImpl implements VendorProductService {
     public List<VendorProduct> createBulk(List<VendorProduct> vendorProducts) {
         return null;
     }
+
+    @Override
+    public VendorProduct getById(String id) {
+        return vendorProductRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "vendor product not found"));
+
+    }
+
+
+
 }
